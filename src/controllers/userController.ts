e31706312ca8f8d610e86dcf3c529ded03c72c80
sohaib-sajjad <91666222+sohaib-sajjad@users.noreply.github.com>
2025-11-21 12:42:47 +0000

    controller to get user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -11,6 +11,16 @@ class UserController {
             res.status(400).json({ message: error.message });
         }
     }
+
+     async getUser(req: Request, res: Response) {
+        try {
+            const user = await User.findById(req.params.id);
+            if (!user) return res.status(404).json({ message: 'User not found' });
+            res.json(user);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
 }
 
 export default new UserController();
