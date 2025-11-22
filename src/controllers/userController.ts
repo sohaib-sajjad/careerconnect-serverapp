311327362f7c0a62c2aaba902fb69e4dcc44ff5c
sohaib-sajjad <91666222+sohaib-sajjad@users.noreply.github.com>
2025-11-22 22:01:29 +0000

    setup controller to get all users

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -12,11 +12,21 @@ class UserController {
         }
     }
 
-     async getUser(req: Request, res: Response) {
+    async getUser(req: Request, res: Response) {
         try {
             const user = await User.findById(req.params.id);
             if (!user) return res.status(404).json({ message: 'User not found' });
             res.json(user);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+
+    async getAllUsers(req: Request, res: Response) {
+        try {
+            const users = await User.find();
+            if (!users) return res.status(404).json({ message: 'Users not found' });
+            res.json(users);
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
